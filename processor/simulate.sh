40299033fb998e625bf61342620ce6a7e1b730da
Antros Economides <ae457@cam.ac.uk>
2022-05-31 15:37:27 +0100

    Enable all iverilog warnings

--- processor/simulate.sh
+++ processor/simulate.sh
@@ -3,7 +3,7 @@ cd sail-core/verilog
 
 VERILOG_FILES=`ls -l *.v | awk '{ print "-l "$9 }'`
 
-iverilog -s top -D SIMULATION_MODE=1 $VERILOG_FILES ../../toplevel.v
+iverilog -s top -D SIMULATION_MODE=1 -W all $VERILOG_FILES ../../toplevel.v
 vvp a.out -fst
 
 if ! pgrep gtkwave >/dev/null; then
