681d4021541dee5f24093ab6ce2dc82e2bf10e23
Antros Economides <ae457@cam.ac.uk>
2022-06-03 10:46:33 +0100

    Compact data_mem state machine

--- processor/sail-core/verilog/data_mem.v
+++ processor/sail-core/verilog/data_mem.v
@@ -48,14 +48,18 @@ module data_mem(
 	/*
 	 *	Current state
 	 */
-	integer			state = 0;
+	reg			state = 0;
+	reg			operation_buf;
 
 	/*
 	 *	Possible states
 	 */
-	parameter		IDLE = 0;
-	parameter		READ = 2;
-	parameter		WRITE = 3;
+	parameter		IDLE	= 0;
+	parameter		OPERATE = 1;
+
+	parameter		READ	= 0;
+	parameter		WRITE	= 1;
+
 
 	/*
 	 *	Line buffer
@@ -177,25 +181,23 @@ module data_mem(
 
 				if (memread == 1'b1) begin
 					clk_stall <= 1;
-					state <= READ;
+					state <= OPERATE;
+					operation_buf <= READ;
 				end
 				else if (memwrite == 1'b1) begin
 					clk_stall <= 1;
-					state <= WRITE;
+					state <= OPERATE;
+					operation_buf <= WRITE;
 				end
 			end
 
-			READ: begin
+			OPERATE: begin
+				if (operation_buf == READ)
+					read_data <= read_buf;
+				else /* if (operation_buf == WRITE) */
+					data_block[addr_buf_block_addr] <= replacement_word;
+
 				clk_stall <= 0;
-
-				read_data <= read_buf;
-				state <= IDLE;
-			end
-
-			WRITE: begin
-				clk_stall <= 0;
-
-				data_block[addr_buf_block_addr] <= replacement_word;
 				state <= IDLE;
 			end
 		endcase
