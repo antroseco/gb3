dfe870748274d0a30ace9a0e4b0e6124e1eaf9b1
Antros Economides <ae457@cam.ac.uk>
2022-06-05 18:12:19 +0100

    Print total path delay after synthesis

--- processor/process_yosys.sh
+++ processor/process_yosys.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -f results.txt
-grep -A16 'Info: Device utilisation:' $1 | tee >(cat >> results.txt)
+grep -A16 'Info: Device utilisation:' $1 | tee -a results.txt
 
 grep -A999 'Info: Running simulated annealing placer for refinement.' $1 \
-	| grep 'Info:   at iteration' | tail -n1 | tee >(cat >> results.txt)
+	| grep 'Info:   at iteration' | tail -n1 | tee -a results.txt
+
+grep 'Total path delay:' $1 | tee -a results.txt
